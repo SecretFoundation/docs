5fde85d430c98040f3a3083e5622094db1af1df8
moonstash <15162813+moonstash@users.noreply.github.com>
2021-09-22 11:38:37 -0700

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -80,8 +80,8 @@ module.exports = {
                 path: "/node-guides/setup-sgx",
               },
               {
-                title: "Secret 3 Full Node Workaround",
-                path: "/node-guides/workaround",
+                title: "Run Full Node",
+                path: "/node-guides/run-full-node-mainnet",
               },
               {
                 title: "Basic Security",
